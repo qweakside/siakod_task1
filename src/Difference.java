16d0435650c7ddc68b4ce61930d2fe0734234907
weakside <eav15062007@yandex.ru>
2026-09-12 14:47:32 +0300

    1.0

---
+++ src/Difference.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Difference {
+    public static int[] Pair(){
+        int []arr=Array();
+        int []minPair = new int[2];
+        int minDiff=Integer.MAX_VALUE;
+        for (int i =0;i< arr.length;i++){
+            for (int j=i+1;j< arr.length;j++){
+                if (minDiff>Math.abs(arr[i]-arr[j])) {
+                    minDiff = Math.abs(arr[i] - arr[j]);
+                    minPair[0] = arr[i];
+                    minPair[1] = arr[j];
+                }
+            }
+        }
+        return minPair;
+    }
+    private static int[] Array(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("введите размер массива");
+        int size = scan.nextInt();
+        int[] array = new int[size];
+        System.out.println("вводите элементы");
+        for (int i = 0; i< size; i++){
+            int n = scan.nextInt();
+            array[i]=n;
+        }
+        return array;
+    }
+}
